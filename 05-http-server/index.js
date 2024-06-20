94a98696a974ee19e0b141cb0bcd88a0a35f545a
Abhay Agarwal <Abhay.agarwal@newput.com>
2024-06-20 11:06:12 +0530

    http server

--- 05-http-server/index.js
+++ 05-http-server/index.js
@@ -2,6 +2,7 @@ const http = require("http");
 const fs = require("fs");
 
 //create the server
+
 const myServer = http.createServer((req, res) => {
   const log = `${Date.now()}: New Req recieved \n`;
   fs.appendFile("log.txt", log, (err, data) => {
@@ -20,4 +21,5 @@ const myServer = http.createServer((req, res) => {
 });
 
 //Listen the server
+
 myServer.listen(8000, () => console.log("Server listning at PORT: 8000"));
