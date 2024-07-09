fa19451eebf56bcbe839748e41ac925bfcec2822
Abhay Agarwal <Abhay.agarwal@newput.com>
2024-07-09 14:50:25 +0530

    cookies

--- 1001-natours-udemy/controllers/authController.js
+++ 1001-natours-udemy/controllers/authController.js
@@ -15,6 +15,17 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOption = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    secure: true,
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production')
+    res.cookie('jwt', token, cookieOption);
+
   res.status(statusCode).json({
     status: 'Success',
     token,
