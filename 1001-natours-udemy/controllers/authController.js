4ced688c112e6945b4ee55ca8c61fc1c59fd4839
Abhay Agarwal <Abhay.agarwal@newput.com>
2024-07-09 12:00:32 +0530

    sending email

--- 1001-natours-udemy/controllers/authController.js
+++ 1001-natours-udemy/controllers/authController.js
@@ -1,6 +1,7 @@
 const { promisify } = require('util');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 
@@ -126,5 +127,37 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your 
+  new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, 
+  please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your Password reset token (valid for 10 min)',
+      message,
+    });
+    res.status(200).json({
+      status: 'success',
+      message: 'Token send to the mail',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    // return next(
+    //   new AppError(
+    //     'There was an error sending the email. Send again later',
+    //     500
+    //   )
+    // );
+
+    throw new AppError(err, 500);
+  }
 });
 exports.resetPassword = (req, res, next) => {};
--- 1001-natours-udemy/routes/userRoutes.js
+++ 1001-natours-udemy/routes/userRoutes.js
@@ -19,5 +19,5 @@ router.route('/signup').post(authController.signup);
 router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
-router.post('/resetPassword', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 module.exports = router;
